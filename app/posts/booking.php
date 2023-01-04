fde04764e37e52f49c188f58e2dc7b370a81aade
gustavenoksson <gustav.enoksson@live.se>
2023-01-04 16:28:04 +0100

    updated booking.php/dates now check

--- app/posts/booking.php
+++ app/posts/booking.php
@@ -3,18 +3,47 @@
 declare(strict_types=1);
 
 require "../hotelFunctions.php";
-header('Content-Type: application/json');
-$db = connect("db/bookings.db");
 
-$arrival = trim($_POST["arrival"]);
+$arrival = ($_POST["arrival"]);
 $departure = trim($_POST["departure"]);
 $roomId = trim($_POST["room"]);
 
+function isValidDate() {
+
+    $arrival = trim($_POST["arrival"]);
+
+    $db = connect("db/bookings.db");
+
+    $bookedDates = array();
+
+    $statement = $db->query("SELECT arrival_date, departure_date FROM bookings");
+    $dates = $statement->fetchAll();
+
+    foreach($dates as $date){
+        // die(var_dump(date("Y-m-d", strtotime($date["arrival_date"]))));
+        // $arrivalDate = date("Y-m-d", strtotime($date["arrival_date"]));
+        // $departureDate = date("Y-m-d", strtotime($date["departure_date"]));
+
+        $arrivalDate = $date["arrival_date"];
+        $departureDate = $date["departure_date"];
+
+        if ($arrival > $arrivalDate && $arrival < $departureDate) {
+            echo $arrival . "is between" . $arrivalDate . $departureDate . "<br>";
+            break;
+        } else {
+            importData();
+            break;
+        }
+    };
+};
+
+isValidDate();
+
 function importData() {
-if (isset($_POST["arrival"], $_POST["departure"], $_POST["room"])) {
+    if (isset($_POST["arrival"], $_POST["departure"], $_POST["room"])) {
+    $db = connect("db/bookings.db");
 
     $statementBookings = $db->prepare("INSERT INTO bookings (arrival_date, departure_date) VALUES (:arrival_date, :departure_date)");
-
     $statementRoomId = $db->prepare("INSERT INTO booking_rooms (room_id) VALUES (:room_id)");
 
     $statementBookings->bindParam(':arrival_date', $arrival);
@@ -23,23 +52,12 @@ if (isset($_POST["arrival"], $_POST["departure"], $_POST["room"])) {
 
     $statementBookings->execute();
     $statementRoomId->execute();
-};
-};
 
-$receiptFile = "../../receipt.json";
-$receipt = file_get_contents($receiptFile);
-echo $receipt;
-
-function isValidDate(){
-
-    $statement = $db->query("SELECT arrival_date, departure_date FROM bookings");
-    $dates = $statement->fetchAll();
-
-    if ($dates["arrival_date"] === $arrival){
-        importData();
-    } else 
-    echo "Sorry this date has been taken.";
+    $receiptFile = "../../receipt.json";
+    $receipt = file_get_contents($receiptFile);
+    echo $receipt;
 }
+};
 
 // $statement = $db->query("SELECT arrival_date, departure_date FROM bookings");
 // $dates = $statement->fetchAll();
