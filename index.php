02610f2fe783fba37baf3c65e65d8e533194367f
gustavenoksson <gustav.enoksson@live.se>
2023-01-11 15:16:21 +0100

    removed view folder

--- index.php
+++ index.php
@@ -1,8 +1,16 @@
-<?php require __DIR__ . "/view/header.php";
-
+<?php
 require __DIR__ . "/vendor/autoload.php";
 require __DIR__ . "/calendar.php";
 ?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Hotel</title>
+    <link rel="stylesheet" href="css/style.css" />
+  </head>
 
   <body>
     <header></header>
@@ -69,4 +77,7 @@ require __DIR__ . "/calendar.php";
         </form>
       </section>
     </main>
-<?php require __DIR__ . "/view/footer.php";
+    <footer></footer>
+  <script src="script.js"></script>
+  </body>
+</html>
